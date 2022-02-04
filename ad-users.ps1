f22248f41330b655e2d98f0bb72b3ea0ab4119a1
Meghan Fairbanks <meghan.fairbanks@transcore.com>
2022-02-04 13:13:02 -0600

    Update ad-users.ps1

--- ad-users.ps1
+++ ad-users.ps1
@@ -4,24 +4,24 @@
 # Updated:  12/1/21
 
 #variables for mail message
-$from = ""
-$to = @("")
+$from = "meghan.fairbanks@ezpassmn.net"
+$to = @("meghan.fairbanks@transcore.com")
 $subject = "Active Directory Users Query"
 $body = "Monthly AD users query."
-$smtpserver = ""
+$smtpserver = "10.202.101.35"
 
 $filepath = "c:\script\ad-users.csv"
 
 #get all enabled accounts
-Get-ADUser -Filter 'enabled -eq $true' -Properties * | Select-Object Name, LastLogonDate, Enabled, PasswordExpired, AccountExpirationDate, whenCreated | Sort-Object LastLogonDate -Descending | Export-Csv $filepath -NoTypeInformation
+Get-ADUser -Filter 'enabled -eq $true' -Properties * | select Name, LastLogonDate, Enabled, PasswordExpired, AccountExpirationDate, whenCreated | Sort-Object LastLogonDate -Descending | Export-Csv $filepath -NoTypeInformation
 
 #get members of domain admin security group
 "`"`"`n`"Domain Admins`",`"LastLogonDate`",`"Enabled`",`"PasswordExpired`",`"AccountExpirationDate`",`"whenCreated`"" | out-file c:\script\ad-users.csv -Append -Encoding ASCII
-Get-ADGroupMember -Identity "Domain Admins" -Recursive | %{Get-ADUser -Identity $_.distinguishedName -Properties *} | Sort-Object LastLogonDate -Descending | Select-Object Name, LastLogonDate, Enabled, PasswordExpired, AccountExpirationDate, whenCreated | Export-Csv $filepath -Append
+Get-ADGroupMember -Identity "Domain Admins" -Recursive | %{Get-ADUser -Identity $_.distinguishedName -Properties *} | Sort-Object LastLogonDate -Descending | Select Name, LastLogonDate, Enabled, PasswordExpired, AccountExpirationDate, whenCreated | Export-Csv $filepath -Append
 
 #get members of dev admins group
 "`"`"`n`"Dev Admins`",`"LastLogonDate`",`"Enabled`",`"PasswordExpired`",`"AccountExpirationDate`",`"whenCreated`"" | out-file c:\script\ad-users.csv -Append -Encoding ASCII
-Get-ADGroupMember -Identity "Dev Admins" -Recursive | %{Get-ADUser -Identity $_.distinguishedName -Properties *} | Sort-Object LastLogonDate -Descending | Select-Object Name, LastLogonDate, Enabled, PasswordExpired, AccountExpirationDate, whenCreated | Export-Csv $filepath -Append
+Get-ADGroupMember -Identity "Dev Admins" -Recursive | %{Get-ADUser -Identity $_.distinguishedName -Properties *} | Sort-Object LastLogonDate -Descending | Select Name, LastLogonDate, Enabled, PasswordExpired, AccountExpirationDate, whenCreated | Export-Csv $filepath -Append
 
 #send mail message with attachments
 Send-MailMessage -From $from -To $to -Subject $subject -Body $body -SmtpServer $smtpserver -Attachments $filepath
